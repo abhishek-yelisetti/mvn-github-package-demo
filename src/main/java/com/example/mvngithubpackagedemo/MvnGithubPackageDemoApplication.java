8d9d6a1669ddde34c0290ce64c0db1ead2cfb7aa
Abhishek Yelisetti <abhishek.yelisetti@zemosolabs.com>
2023-12-14 14:21:40 +0530

    Added hello world controller

---
+++ src/main/java/com/example/mvngithubpackagedemo/MvnGithubPackageDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.mvngithubpackagedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvnGithubPackageDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvnGithubPackageDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mvngithubpackagedemo/MvnGithubPackageDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mvngithubpackagedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvnGithubPackageDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
